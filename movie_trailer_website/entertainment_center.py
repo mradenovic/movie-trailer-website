a878f81e6294d08b3725bf6acbf50029612903f0
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-07-16 13:49:57 -0400

    feat(website): copy files created during course
    Instead starting from scratch, copy files created during Udacity's
    Programming Foundations with Python Course

---
+++ movie_trailer_website/entertainment_center.py
@@ -0,0 +1,24 @@
+import media
+import fresh_tomatoes
+
+toy_story = media.Movie("Toy Story",
+                        "Story about toys",
+                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+
+avatar = media.Movie("Avatar",
+                     "A marine on an alien planet",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+the_intouchables = media.Movie("The Intouchables",
+                               "The story of the friendship between the two men",
+                               "https://upload.wikimedia.org/wikipedia/en/9/93/The_Intouchables.jpg",
+                               "https://www.youtube.com/watch?v=34WIbmXkewU")
+
+
+movies = [toy_story, avatar, the_intouchables]
+
+fresh_tomatoes.open_movies_page(movies)
+
