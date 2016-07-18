c5b6ceb8aa75b260ff0f6ce42f4d5bd3d6292fbd
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-07-17 22:28:39 -0400

    feat(entertainment_center): update storylines

--- movie_trailer_website/entertainment_center.py
+++ movie_trailer_website/entertainment_center.py
@@ -2,18 +2,24 @@ import media
 import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
-                        "Story about toys",
+                        "A cowboy doll is profoundly threatened and jealous "
+                        "when a new spaceman figure supplants him as top toy in "
+                        "a boy's room.",
                         "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 
 avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
+                     "A paraplegic marine dispatched to the moon Pandora on a "
+                     "unique mission becomes torn between following his orders "
+                     "and protecting the world he feels is his home.",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
 the_intouchables = media.Movie("The Intouchables",
-                               "The story of the friendship between the two men",
+                               "After he becomes a quadriplegic from a "
+                               "paragliding accident, an aristocrat hires a "
+                               "young man from the projects to be his caregiver.",
                                "https://upload.wikimedia.org/wikipedia/en/9/93/The_Intouchables.jpg",
                                "https://www.youtube.com/watch?v=34WIbmXkewU")
 
