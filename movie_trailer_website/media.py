b7e6f3e33777d68792df720784b0e1d06bf4097c
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-07-17 20:57:11 -0400

    docs(media): add docstrings

--- movie_trailer_website/media.py
+++ movie_trailer_website/media.py
@@ -1,8 +1,25 @@
+"""This module contains class definitions for storing media files"""
 import webbrowser
 
 class Movie():
+    """Movie class defines movies.
+
+    Attributes:
+        movie_title (str): Title of the movie
+        movie_storyline (str): Sort description of the movie
+        poster_image (str): Url of the poster image
+        trailer_youtube (str): URL of the Youtube trailer
+    """
 
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        # type: (object, object, object, object) -> object
+        """
+        Arguments:
+            movie_title (str): Title of the movie
+            movie_storyline (str): Sort description of the movie
+            poster_image (str): Url of the poster image
+            trailer_youtube (str): URL of the Youtube trailer
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
